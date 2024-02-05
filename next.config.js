32225a81e50283d50453f168b1ead8f5fdef7f16
git config --global user.name 'vasiliy.shevchuk <git config --global user.email 'vasiliy.shevchuk@chisw.com>
2024-02-05 12:16:37 +0200

    - fix domains

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async redirects() {
-    return [
-      {
-        source: '/(.*)',
-        destination: 'https://netflix-clone-v-shef.vercel.app/',
-        permanent: true,
-      },
-    ]
-  },
   reactStrictMode: true,
   images: {
     remotePatterns: [
